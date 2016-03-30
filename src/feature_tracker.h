aca57fbfd7587c44dc3543a842399471c342b4cd
qintong <qintonguav@gmail.com>
2016-03-30 21:03:53 +0800

    fix small debugs

--- src/feature_tracker.h
+++ src/feature_tracker.h
@@ -33,7 +33,7 @@
 #include "camodocal/camera_models/CameraFactory.h"
 #include "camodocal/camera_models/CataCamera.h"
 
-#define MIN_DIST 25
+#define MIN_DIST 30
 #define MAX_CNT 100
 #define FREQ 2
 #define RANSAC true
